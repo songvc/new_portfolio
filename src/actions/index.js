ec8fec23793d0f243215d81465ac728305093c38
Vincent Song <songvc@users.noreply.github.com>
2016-07-06 21:01:10 -0700

    create auth/unauth actinos to toggle authenticated boolean

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,25 @@
+import axios from 'axios';
+import { browserHistory } from 'react-router';
+import { AUTH_USER } from './type';
+
 const ROOT_URL = 'http://localhost:3090';
 
 export function signinUser({ email, password }) {
   return function(dispatch) {
-    dispatch({ type: })
     // Submit email/password to the server
-    
-    // If request is good...
-    // - Update state to indicate user is authenticated
-    // - Save the JWT token
-    // - Redirect to the route /Main
+    axios.post(`${ROOT_URL}/signin`, { email, password })
+      .then(response => {
+        // If request is good...
+        // - Update state to indicate user is authenticated
+        dispatch({ type: AUTH_USER })
+        // - Save the JWT token
+        // - Redirect to the route /feature
+        browserHistory.push('/feature');
+      })
+      .catch(() => {
+
+      })
+
 
     // IF request is bad...
     // - Show an error to the user
--- src/components/Admin/Signin.js
+++ src/components/Admin/Signin.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
+import * as actions from '../../actions';
 
 const fields = [ 'email', 'password' ];
 
@@ -7,6 +8,7 @@ class Signin extends Component {
 
   handleFormSubmit({ email, password }) {
     console.log(email, password);
+    this.props.signinUser({ email, password });
   }
 
   render() {
@@ -14,23 +16,28 @@ class Signin extends Component {
     const {  fields: { email, password }, handleSubmit } = this.props;
 
     return (
-      <form className="ui form" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-        <div className="field">
-          <label>Email:</label>
-          <input {...email} type="text" placeholder="Email" />
-        </div>
-        <div className="field">
-          <label>Password</label>
-          <input {...password} type="text" placeholder="Password" />
-        </div>
-        <div className="field">
-          <div className="ui checkbox">
-            <input type="checkbox" tabIndex="0" className="hidden" />
-            <label>I agree to the Terms and Conditions</label>
+      <div>
+        <h3 className="ui block header">
+          Signin
+        </h3>
+        <form className="ui form" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+          <div className="field">
+            <label>Email:</label>
+            <input {...email} type="text" placeholder="Email" />
           </div>
-        </div>
-        <button className="ui button" type="submit">Submit</button>
-      </form>
+          <div className="field">
+            <label>Password</label>
+            <input {...password} type="text" placeholder="Password" />
+          </div>
+          <div className="field">
+            <div className="ui checkbox">
+              <input type="checkbox" tabIndex="0" className="hidden" />
+              <label>I agree to the Terms and Conditions</label>
+            </div>
+          </div>
+          <button className="ui button" type="submit">Submit</button>
+        </form>
+      </div>
     )
   }
 }
@@ -38,4 +45,4 @@ class Signin extends Component {
 export default reduxForm({
   form: 'simple',
   fields
-})(Signin);
+}, null, actions)(Signin);
