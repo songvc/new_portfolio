35d552451f45267e85fb47ab5bcbf5c426d29576
Vincent Song <songvc@users.noreply.github.com>
2016-07-21 15:01:34 -0700

    page header

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,16 +1,2 @@
 import React, { Component } from 'react';
 import { StyleSheet, css } from 'aphrodite';
-
-// const styles = StyleSheet.create({
-//
-// });
-//
-// const PageHeader = () => {
-//   return (
-//
-//   )
-// }
-//
-// PageHeader.propTypes = {
-//
-// }
