30bbf3dfb05246c4787fdc69af59680a3caabc5e
Vincent Song <songvc@users.noreply.github.com>
2016-08-17 20:21:57 -0700

    add blog lists

---
+++ src/components/Blog/BlogList.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { markdown } from 'markdown';
+
+class BlogList extends Component {
+  render() {
+    const rawHTML = markdown.toHTML(this.props.bin.content);
+    return (
+      <div className="col-xs-4">
+        <h5>Output</h5>
+        <div dangerouslySetInnerHTML={{ __html: rawHTML}}></div>
+      </div>
+      <div className="col-xs-4">
+        <h5>Output</h5>
+        <div dangerouslySetInnerHTML={{ __html: rawHTML}}></div>
+      </div>
+      <div className="col-xs-4">
+        <h5>Output</h5>
+        <div dangerouslySetInnerHTML={{ __html: rawHTML}}></div>
+      </div>
+      <div className="col-xs-4">
+        <h5>Output</h5>
+        <div dangerouslySetInnerHTML={{ __html: rawHTML}}></div>
+      </div>
+    )
+  }
+}
+
+export default BlogList;
